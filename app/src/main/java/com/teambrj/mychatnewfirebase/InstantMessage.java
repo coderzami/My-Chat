e8d2c8e189b452f49c8e8d12058d73dcb76ccb6f
Team BRJ <zamiur53@gmail.com>
2018-03-14 16:23:21 +0600

    My first messenger app.....Though it is a basic messenger app....but i built it top to bottom......

---
+++ app/src/main/java/com/teambrj/mychatnewfirebase/InstantMessage.java
@@ -0,0 +1,26 @@
+package com.teambrj.mychatnewfirebase;
+
+/**
+ * Created by User on 3/12/2018.
+ */
+
+public class InstantMessage {
+    private String message;
+    private String author;
+
+    public InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
